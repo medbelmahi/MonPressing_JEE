638873b4dafc60cd6edcf1ac8ac51279967dc048
Mohamed BELMAHI <medbelmahi@outlook.com>
2015-12-06 12:42:33 +0000

    Revert "dd"
    This reverts commit b05209139357d3471d2177b6be1f74ad54fef18e.

--- ecommerce.app/src/main/java/ma/pressing/ecommerce/controller/customer/CustomerSignInUpController.java
+++ ecommerce.app/src/main/java/ma/pressing/ecommerce/controller/customer/CustomerSignInUpController.java
@@ -29,58 +29,19 @@ import ma.pressing.ecommerce.web.form.data.CustomerForm;
 @Controller
 public class CustomerSignInUpController {
 
-	public static String PAGE_CONNEXION = "pages/customer_connexion";
 	public static String PAGE_INSCRIPTION = "pages/customer_inscription";
-	
-	@Resource(name = "customerFormValidator")
-	Validator customerFormValidator;
+	public static String PAGE_CONNEXION = "pages/customer_connexion";
 	
 	@Autowired
 	DefaultCustomerFacade cutomerFacade;
 	
-	
-	@RequestMapping(value="/NouvelleInscription", method = RequestMethod.POST)
-	public String addUserForm(@ModelAttribute("userForm") @Validated final CustomerForm userForm, final BindingResult result, final ModelMap model) {
-		
-		customerFormValidator.validate(userForm, result);
-		
-		if(result.hasErrors()){
-			
-			populateDefaultUserFormAttribute(model);
-			model.addAttribute("userForm", userForm);
-			return PAGE_INSCRIPTION;
-		}
-		
-		final CustomerData customerData = cutomerFacade.addNewCustomer(userForm);
-		
-		model.addAttribute("customer", customerData);
-		
-		return PAGE_CONNEXION;
-	}
-	
-	// customize the error message
-	private String getErrorMessage(final HttpServletRequest request, final String key) {
-
-		final Exception exception = (Exception) request.getSession().getAttribute(key);
-
-		String error = "";
-		if (exception instanceof BadCredentialsException) {
-			error = "Email ou Mot de passe incorrect !";
-		} else if (exception instanceof LockedException) {
-			error = exception.getMessage();
-		} else {
-			exception.printStackTrace();
-			error = "Votre compte est désactivé";
-		}
-
-		return error;
-	}
-	
+	@Resource(name = "customerFormValidator")
+	Validator customerFormValidator;
 	
 	
 	@RequestMapping(value="/Inscription", method = RequestMethod.GET)
-	public String inscription(final ModelMap model){
-		final CustomerForm userForm = new CustomerForm();
+	public String inscription(ModelMap model){
+		CustomerForm userForm = new CustomerForm();
 		
 		model.addAttribute("userForm", userForm);
 
@@ -88,12 +49,50 @@ public class CustomerSignInUpController {
 
 		return PAGE_INSCRIPTION;
 	}
+	
+	@RequestMapping(value="/NouvelleInscription", method = RequestMethod.POST)
+	public String addUserForm(@ModelAttribute("userForm") @Validated CustomerForm userForm, BindingResult result, ModelMap model) {
+		
+		customerFormValidator.validate(userForm, result);
+		
+		if(result.hasErrors()){
+			populateDefaultUserFormAttribute(model);
+			model.addAttribute("userForm", userForm);
+			return PAGE_INSCRIPTION;
+		}
+		
+		CustomerData customerData = cutomerFacade.addNewCustomer(userForm);
+		
+		model.addAttribute("customer", customerData);
+		
+		return PAGE_CONNEXION;
+	}
+	
+	
+	
+	private void populateDefaultUserFormAttribute(ModelMap model) {
+		Map<String, String> civilityList = new LinkedHashMap<String, String>();
+		civilityList.put(CivilityType.MR.toString(), "Monsieur");
+		civilityList.put(CivilityType.MRS.toString(), "Madame");
+		civilityList.put(CivilityType.MISS.toString(), "Demoiselle");
+		model.addAttribute("civilityList", civilityList);
+		
+		Map<String, String> country = new LinkedHashMap<String, String>();
+		country.put(CityDeMaroc.Casablanca.toString(), "Casablanca One");
+		model.addAttribute("cityList", country);
+		
+		Map<String, String> typeCompte = new LinkedHashMap<>();
+		typeCompte.put("false", "Particulier");
+		typeCompte.put("true", "Professionnel");
+		model.addAttribute("typeCompte", typeCompte);
+
+	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login(@RequestParam(value = "error", required = false) final String error,
-			@RequestParam(value = "logout", required = false) final String logout, final HttpServletRequest request) {
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout, HttpServletRequest request) {
 
-		final ModelAndView model = new ModelAndView();
+		ModelAndView model = new ModelAndView();
 		if (error != null) {
 			model.addObject("error", getErrorMessage(request, "SPRING_SECURITY_LAST_EXCEPTION"));
 		}
@@ -107,22 +106,22 @@ public class CustomerSignInUpController {
 
 	}
 	
-	private void populateDefaultUserFormAttribute(final ModelMap model) {
-		final Map<String, String> civilityList = new LinkedHashMap<String, String>();
-		civilityList.put(CivilityType.MR.toString(), "Monsieur");
-		civilityList.put(CivilityType.MRS.toString(), "Madame");
-		civilityList.put(CivilityType.MISS.toString(), "Demoiselle");
-		model.addAttribute("civilityList", civilityList);
-		
-		final Map<String, String> country = new LinkedHashMap<String, String>();
-		country.put(CityDeMaroc.Casablanca.toString(), "Casablanca One");
-		model.addAttribute("cityList", country);
-		
-		final Map<String, String> typeCompte = new LinkedHashMap<>();
-		typeCompte.put("false", "Particulier");
-		typeCompte.put("true", "Professionnel");
-		model.addAttribute("typeCompte", typeCompte);
+	// customize the error message
+	private String getErrorMessage(HttpServletRequest request, String key) {
 
+		Exception exception = (Exception) request.getSession().getAttribute(key);
+
+		String error = "";
+		if (exception instanceof BadCredentialsException) {
+			error = "Email ou Mot de passe incorrect !";
+		} else if (exception instanceof LockedException) {
+			error = exception.getMessage();
+		} else {
+			exception.printStackTrace();
+			error = "Votre compte est désactivé";
+		}
+
+		return error;
 	}
 	
 }
